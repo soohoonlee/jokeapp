85156945cc1e27ef54ae48aa3ad7c19695bc22b4
soohoonlee <jjinimania@gmail.com>
2019-01-22 00:53:05 +0900

    Initial commit

---
+++ src/main/java/me/ssoon/joke/JokeappApplication.java
@@ -0,0 +1,14 @@
+package me.ssoon.joke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeappApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JokeappApplication.class, args);
+  }
+
+}
+
